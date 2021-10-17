1694a399dc4e286bc9f987f805f7c1901a372a88
LeviBcodes <90584200+LeviBcodes@users.noreply.github.com>
2021-10-17 17:53:06 -0600

    adding navigation links to navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,7 +3,19 @@ import './App.css';
 
 const Navbar = () => {
     return (
-        <h1>Navbar header</h1>
+        <nav>  
+            <div className='nav-link-container'>
+                <img src={logo} alt='' className='logo' />
+                <ul className='nav-links'>
+                    <li>
+                        <a href="/">Home</a>
+                    </li>
+                    <li>
+                        <a href="/about">About</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     )
 }
 
