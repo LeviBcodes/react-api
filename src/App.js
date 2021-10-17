b7d5f470768cdcdbd7f6e80bd55062ca1f5ff30f
LeviBcodes <90584200+LeviBcodes@users.noreply.github.com>
2021-10-17 17:46:20 -0600

    setting up navbar component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-          Learn React
-      </header>
+      <Navbar />
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './App.css';
+
+const Navbar = () => {
+    return (
+        <h1>Navbar header</h1>
+    )
+}
+
+export default Navbar;
