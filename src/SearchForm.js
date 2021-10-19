1a495aaaee0448bbe1b65a945bc20a788d4bab86
LeviBcodes <90584200+LeviBcodes@users.noreply.github.com>
2021-10-18 23:01:27 -0800

    adding search form to search component

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,6 +1,15 @@
 import React from 'react'
 import { useGlobalContext } from '../context'
 const SearchForm = () => {
+    return (
+        <section>
+          <form>
+            <div>
+              <input/>
+            </div>
+          </form>
+        </section>
+      )
 }
 
 export default SearchForm
