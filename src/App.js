15c865c8a0b46b83fead0cc70531c5d0c332432e
LeviBcodes <90584200+LeviBcodes@users.noreply.github.com>
2021-10-18 23:04:36 -0800

    importing search form to main app compontent

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './Navbar';
+import SearchForm from './SearchForm';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <SearchForm />
     </div>
   );
 }
